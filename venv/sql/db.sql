2d5f15257f2ea7676546b60a1cbfecf761f7fdc5
pyp79 <pyp79@sina.com>
2018-06-12 00:08:09 +0800

    20180611

--- venv/sql/db.sql
+++ venv/sql/db.sql
@@ -1,6 +1,5 @@
 docker run --name easyump-mysql -v /opt/mysql_data:/var/lib/mysql -p 3333:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:latest --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
-
 创建模式
 CREATE SCHEMA `ump` DEFAULT CHARACTER SET utf8 ;
 
